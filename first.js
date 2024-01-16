14f7c63a12b168ca8f09e48c75acce6d498dd89c
Yogesh3712 <113026686+Yogesh3712@users.noreply.github.com>
2024-01-17 00:10:14 +0530

    city of my dreams

---
+++ first.js
@@ -0,0 +1,15 @@
+let a= 5;
+let b=2;
+console.log("a=",a, "& b = ",b);
+
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a%b=",a%b);
+console.log("a**b=",a**b)
+//unary 
+
+
+console.log("a++=",a++);
+console.log("a=",a);
+//assignment operator
